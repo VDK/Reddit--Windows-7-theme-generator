b4b99460c5bee67619ad69a40b93de42411f2554
vdk <veradekok@gmail.com>
2011-06-29 21:28:17 +0200

    first commit

---
+++ rss.php
@@ -0,0 +1,57 @@
+<?php
+header ("Content-Type:text/xml");  
+if(empty($_GET['sub'])) {
+  $subreddit = 'EarthPorn';
+}
+else {
+     $subreddit = $_GET['sub'];
+}
+$json = json_decode(file_get_contents("http://www.reddit.com/r/".$subreddit."/.json",0,null,null), true);
+echo '
+<rss version="2.0">
+<channel>
+<title>Reddit Eartporn Windows 7 theme rss feed</title>
+<link>http://veradekok.nl/win7theme/rss.php</link>
+<description></description>
+ <ttl>Reddit '.$subreddit.' win7 theme</ttl>';
+foreach ($json['data']['children'] as $child){
+  $extension = substr($child['data']['url'],-4);  
+  if ($extension == '.jpg' || $extension == '.png' || $extension =='.gif' || $extension == 'jpeg'){
+    item_start ($child['data']);
+    switch ($extension){ 
+      case '.jpg':
+      case 'jpeg': 
+                  $data_type = 'image/jpeg';
+                  break; 
+       case '.png':
+                  $data_type = 'image/png';
+                  break;
+       case '.gif':
+                  $data_type = 'image/gif';
+                  break;
+     }
+     echo   ' <enclosure url="'.$child['data']['url'].'" type="'.$data_type.'"/>
+     
+          <description>Reddit '.$subreddit.' win7 theme</description>
+            </item>';
+  }
+  else if ( $child['data']['domain'] == 'imgur.com'){
+    item_start ($child['data']);
+    echo '<enclosure url="'.$child['data']['url'].'.jpg" type="image/jpeg" />
+    
+          <description>Reddit EarthPorn win7 theme</description>
+          </item>';
+  }
+}
+  
+  echo '</channel></rss>';
+  
+  function item_start($item){
+    echo '<item> 
+          <guid>'.$item['id'].'</guid>
+          <title>'.$item['title'].'</title>
+          <pubDate>'.date(DATE_ATOM,$item['created']).'</pubDate>
+          <link ref=\''.$item['url'].'\'/>
+          ';
+          
+  }
